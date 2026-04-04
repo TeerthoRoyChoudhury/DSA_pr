c6f927193798f14c8d48e112677d4425ea1b0ebb
Teertho Roychoudhury <roychoudhuryteertho96@gmail.com>
2026-04-04 14:32:27 +0530

    matrix

---
+++ 2darray/basic.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int matrix[4][3];
+    int row = 4;
+    int col = 3;
+
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<col;j++){
+            cin>>matrix[i][j];
+        }
+    }
+
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<col;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+
+}
+
+
